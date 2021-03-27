2dcbc6194979f7cd839810bed9536a4a2cf9678b
HYEONSEOK JANG <hsjang@HYEONSEOKui-MacBookAir.local>
2021-03-27 18:40:33 +0900

    modify1

--- test.java
+++ test.java
@@ -5,12 +5,10 @@ public class test {
 
     public static void main(String[] args) {
 
-        int a = 10;
-        int b = 20;
-        int c = a+b;
-        System.out.println("결과를 출력합니다.");
-        System.out.println(c);
         System.out.println("Mac Java 환경 TEST");
+        System.out.println("Mac VSCODE JAVA TEST");
+        System.out.println("Mac Git Test for Terminal");
+    
     }
     
         
