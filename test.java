15de96dc8143eb7062083331becd996f7975cf41
HYEONSEOK JANG <hsjang@HYEONSEOKui-MacBookAir.local>
2021-03-27 18:33:21 +0900

    test

---
+++ test.java
@@ -0,0 +1,17 @@
+/**
+ * test
+ */
+public class test {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        int c = a+b;
+        System.out.println("결과를 출력합니다.");
+        System.out.println(c);
+        System.out.println("Mac Java 환경 TEST");
+    }
+    
+        
+}
